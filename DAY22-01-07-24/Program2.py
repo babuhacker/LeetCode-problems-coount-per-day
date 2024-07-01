da1d33b6fda0a57dfbe62df97908da178976e78d
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-07-01 20:39:49 +0530

    Add files via upload

---
+++ DAY22-01-07-24/Program2.py
@@ -0,0 +1,17 @@
+# Maximum Product Subarray
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        result = max(nums)
+        curMin , curMax = 1,1
+
+        for n in nums:
+            if n == 0:
+                curMin, curMax = 1, 1
+                continue
+            tmp = curMax * n
+            curMax = max(n * curMax, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
+            result = max(result, curMax)
+
+        return result
